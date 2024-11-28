30b7044ad7291c5d8ac592076605e8dd3ffa6124
Arthur Bellec <arthur.bellec@gmail.com>
2024-11-28 16:48:57 +0000

    add a content for the footer

---
+++ src/Conponents/Body.jsx
@@ -0,0 +1,6 @@
+export function Body() {
+    return (
+        <div> Je suis le Body </div>
+        
+    )
+}
--- src/Conponents/Footer.jsx
+++ src/Conponents/Footer.jsx
@@ -1,5 +1,26 @@
+import styles from "./Footer.module.css";
+import Logobds from "./logobds.png";
+import logoinsta from "./logo-insta.jpeg";
+
 export function Footer(){
     return (
-        <div>Im a footer </div>
+        <div className={styles["footer"]}>
+            <div className={styles["container"]}>
+                <div className={styles["footer-top"]}>
+                    <img src={Logobds} className={styles["logo-bds"]}></img>
+                </div>
+                <div className={styles["contact"]}>
+                    <h2> Suivez-nous: </h2>
+                    <ul className={styles["liste-logo"]}>
+                        <li className={styles["logo-item"]}>
+                            <a href="https://www.instagram.com/bds_nextu/?next=%2F" target="_blank">
+                                <img src={logoinsta} className={styles["insta"]}></img>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
